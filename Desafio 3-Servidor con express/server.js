982c793c517a0003faef9f7ed019549599fa70c6
flor perez <flor.franso@live.com>
2022-11-30 13:50:06 -0300

    entrega  terminanda

--- Desafio 3-Servidor con express/server.js
+++ Desafio 3-Servidor con express/server.js
@@ -1,25 +1,6 @@
-const express = require ("express");
-const Contenedor = require ("./manejoArchivos")
+const express = require("express");
+const Contenedor = require("./manejoArchivos");
 
-
-const app = express();
-const PORT= 8080;
-
-app.get("/productos", async (req, res)=>{
-//codigo para obtener todos los productos
-})
-
-app.get('/productosRandom', async (req, res)=> {
-    //codigo para obtener prod random
-})
-
-const server = app.listen(PORT, () => {
-    console.log(`Server listening: PORT ${PORT}`);
-})//codigo para levantar el servidor en el puerto
-
-server.on('error', error => console.log(`Error en servidor $(error)`))
-/*const express = require("express");
-const Contenedor = require("./indexManejoArchivos");
 
 const app = express();
 const PORT = 8080;
@@ -28,17 +9,17 @@ const contenedor = new Contenedor("productos.txt");
 
 app.get("/productos", async (req, res) => {
     const productos = await contenedor.getAll();
-    res.send(productos);
-});
+    res.send({Productos: productos});
+})
 
-app.get("/productoRandom", async (req, res) => {
+app.get('/productoRandom', async (req, res) => {
     const productos = await contenedor.getAll();
-    let numeroRandom = Math.floor(Math.random()*productos.length);
-    res.send(productos[numeroRandom]);
-});
+    let numeroRandom = parseInt(Math.random() * productos.length);
+    res.send({Productos: productos[numeroRandom]});
+}) 
 
 const server = app.listen(PORT, () => {
     console.log(`Server listening: PORT ${PORT}`);
-});
+})
 
-server.on(`error`, err => console.log(`error: ${err}`));*/
+server.on('error', error => console.log(`Error en servidor ${error}`))
